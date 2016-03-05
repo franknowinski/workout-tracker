23c9bdca637f589f03426c7f04f99b4c7426fc0f
frankNowinski <nowinski.frank@gmail.com>
2016-03-05 15:37:10 -0500

    abstract away muscle group method"

--- NOTES.md
+++ NOTES.md
@@ -16,9 +16,3 @@ routine
 
 execrsizes
   "bench press"    "chets"
-
-1. Update Exercise form to use muscle_group_name
-2. Add name to Workout Plan
-3. Update actions
-4. Exercise Validations
-5. Abstract away muscle groups
--- app/models/workout_plan.rb
+++ app/models/workout_plan.rb
@@ -3,48 +3,25 @@ class WorkoutPlan < ActiveRecord::Base
   has_many :exercises
   has_many :workouts, through: :exercises
 
+  MUSCLE = {
+    "chest" => 1,
+    "back" => 2,
+    "legs" => 3,
+    "shoulders" => 4,
+    "bis" => 5,
+    "tris" => 6,
+    "abs" => 7,
+  }
+
   def all_muscle_groups
     self.exercises.collect{ |exercise| exercise.muscle_group.name.downcase }.uniq
   end
 
-  NAMES = {
-    1 => "Chest",
-    "Chest" => 2
-  }
-
-  def exercises_by_group(name)
-    exercises.where(muscle_group_id: NAMES[name])
+  def muscle_group_by(name)
+    exercises.where(muscle_group_id: MUSCLE[name])
   end
 
   def created_date
     created_at.strftime("%A, %b %d")
-  end
-
-  def chest_exercises
-    exercises.where(muscle_group_id: 1)
-  end
-
-  def back_exercises
-    exercises.where(muscle_group_id: 2)
-  end
-
-  def legs_exercises
-    exercises.where(muscle_group_id: 3)
-  end
-
-  def shoulders_exercises
-    exercises.where(muscle_group_id: 4)
-  end
-
-  def bis_exercises
-    exercises.where(muscle_group_id: 5)
-  end
-
-  def tris_exercises
-    exercises.where(muscle_group_id: 6)
-  end
-
-  def abs_exercises
-    exercises.where(muscle_group_id: 7)
   end
 end
