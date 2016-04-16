fb951f244b6fcd6be7265136fa497bb54b1ca1c1
frankNowinski <nowinski.frank@gmail.com>
2016-04-16 02:48:43 -0400

    add validations

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,31 +19,53 @@ function attachListeners(){
     event.preventDefault();
   })
 
-  // Abstracted away to use generalized form - create exercise form
-  // $('#exercise-form').submit(function(event){
-  //   event.preventDefault();
-  //
-  //   // REMOVED TO UTILIZE REMOTE TRUE
-  //   // var form_method = $(this).attr('method');
-  //   // var form_action = $(this).attr('action');
-  //   //
-  //   //
-  //   // $.ajax(form_action, {
-  //   //   type: form_method,
-  //   //   dataType: 'script',
-  //   //   data: $(this).serialize()
-  //   // })
-  // });
+  $('#new_exercise').bind("ajax:error", function(evt, xhr, status, error){
 
-  // Abstracted away to use generalized form - edit workout form
-  // $('form.edit_workout').submit(function(event){
+    var $form = $(this),
+        errors,
+        errorText;
+    try {
+      // Populate errorText with the comment errors
+      errors = $.parseJSON(xhr.responseText);
+    } catch(err) {
+      // If the responseText is not valid JSON (like if a 500 exception was thrown), populate errors with a generic error message.
+      errors = {message: "Please reload the page and try again"};
+    }
+
+    // Build an unordered list from the list of errors
+    errorText = "There were errors with the submission: \n<ul>";
+
+    for ( error in errors ) {
+      errorText += "<li>" + error + ': ' + errors[error] + "</li> ";
+    }
+
+    errorText += "</ul>";
+
+    $form.find('div.field_with_errors').html(errorText);
+
+    // $form.find('div.validation-error').html(errorText);
+  });
+
+
+  // Abstracted away to use Remote True - create exercise form
+  // $('#new_exercise').submit(function(event){
   //   event.preventDefault();
-  // })
+  //   var form_method = $(this).attr('method');
+  //   var form_action = $(this).attr('action');
+  //
+  //   $.ajax(form_action, {
+  //     type: form_method,
+  //     dataType: 'script',
+  //     data: $(this).serialize()
+  //   }).error(function(res){
+  //     debugger;
+  //   })
+  // });
 
   $('tbody td a').click(function(event){
     event.preventDefault();
 
-    // REMOVED TO UTILIZE REMOTE TRUE
+    // REMOVED TO USER REMOTE TRUE
     // var method = $(this).data('method');
     // var deleteURL = $(this).attr('href');
     //
@@ -55,19 +77,19 @@ function attachListeners(){
 
   $('tbody').on('click', 'a.edit-link', function(){
     var workoutRow = $(this).parents('tr')
-
+    // $('input[name="workout[name]"]').focus()
     // Hide workout values and display edit workout input form
     $('span', workoutRow).addClass('hide-row');
     $('input', workoutRow).attr('id', 'edit-workout')
   });
 
-  $('tbody').on('blur', 'a.edit-link', function(){
-    var workoutRow = $(this).parents('tr')
-
-    // Display workout values and hide edit workout input form
-    $('span', workoutRow).removeClass('hide-row');
-    $('input', workoutRow).removeAttr('id');
-  });
+  // $('tbody').on('blur', 'a.edit-link', function(){
+  //   var workoutRow = $(this).parents('tr')
+  //
+  //   // Display workout values and hide edit workout input form
+  //   $('span', workoutRow).removeClass('hide-row');
+  //   $('input', workoutRow).removeAttr('id');
+  // });
 }
 
 $(function(){
--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -18,10 +18,14 @@ class ExercisesController < ApplicationController
         format.html { render :new }
         format.js { }
       else
-        format.html { render action: 'new' }
-        format.json { render json: @exercise.errors}
+        # format.html { render action: 'new' }
+        format.html { redirect_to new_workout_plan_exercise_path(current_plan), notice: 'Failed' }
+        format.js { render json: @exercise.errors}
+        # format.json { render json: { error: @exercise.errors }, status: 422 }
+        # render new_workout_plan_exercise_path(current_plan)
       end
     end
+
   end
 
   private
