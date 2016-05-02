5245a127652c05262ff37e173bfdd7ed427f5c00
frankNowinski <nowinski.frank@gmail.com>
2016-05-02 17:04:33 -0400

    enabled user to post multiple comments

--- app/assets/javascripts/browse_plans.js
+++ app/assets/javascripts/browse_plans.js
@@ -1,14 +1,47 @@
+function constructTableHeader() {
+  return [
+    '<div class="col-sm-8">',
+      '<table class="table table-hover clearfix">',
+        '<thead>',
+          '<tr>',
+            '<th class="col-sm-4">Exercise</th>',
+            '<th class="col-sm-3">Sets</th>',
+            '<th class="col-sm-3">Reps</th>',
+          '</tr>',
+        '</thead>',
+      '<tbody>',
+    '</div>'
+  ].join('');
+};
+
+function constructMuscleGroup(muscleGroup) {
+  return [
+    '<div class="row ' + muscleGroup.name.toLowerCase() + '-group table-rows">',
+      '<div class="container col-sm-3">',
+        '<h2 class="browse-muscle-header">' + muscleGroup.name + '</h2><br>',
+      '</div>'
+  ].join('');
+};
+
+function constructWorkoutRow(workout) {
+  return [
+    '<tr class="browse-workout-row">',
+      '<td>' + workout.name + '</td>',
+      '<td>' + workout.sets + '</td>',
+      '<td>' + workout.reps + '</td>',
+    '</tr>'
+  ].join('');
+}
+
 function contstructTable(muscleGroup, workouts) {
   if ($('.' + muscleGroup.name.toLowerCase() + '-group').length == 0){
 
-    var muscleHeader ='<div class="row ' + muscleGroup.name.toLowerCase() + '-group table-rows"><div class="container col-sm-3"><h2 class="browse-muscle-header">' + muscleGroup.name + '</h2><br></div>';
-
-    var workoutsHeader = '<div class="col-sm-8"><table class="table table-hover clearfix"><thead><tr><th class="col-sm-4">Exercise</th><th class="col-sm-3">Sets</th><th class="col-sm-3">Reps</th></tr></thead><tbody></div>';
+    var muscleHeader = constructMuscleGroup(muscleGroup)
+    var workoutsHeader = constructTableHeader();
 
     var workoutRows = '';
-
     workouts.forEach(function(workout){
-      workoutRows += '<tr class="browse-workout-row"><td>' + workout.name + '</td><td>' + workout.sets + '</td><td>' + workout.reps + '</td></tr>';
+      workoutRows += constructWorkoutRow(workout);
     });
 
     var html = muscleHeader += workoutsHeader += workoutRows += '</tbody></table></div>';
@@ -27,12 +60,46 @@ function removeElements() {
   $('blockquote').remove();
 }
 
+// Displays blockquote
 function constructComment(comment) {
   var date = new Date(comment.created_at).toString().slice(0, 10);
   var user = comment.user.name || comment.user.email;
-  var commentRow = '<blockquote><p>' + comment.content +'</p><footer>' + user + '<cite> ' + date + '</cite></footer></blockquote>';
 
-  return commentRow;
+  return [
+    '<blockquote>',
+      '<p>' + comment.content +'</p>',
+      '<footer>' + user + '<cite> ' + date + '</cite></footer>',
+    '</blockquote>'
+  ].join('');
+}
+
+function constructCommentForm(formAction) {
+  return [
+    '<div class="field-with-errors"></div>',
+      '<form action="' + formAction + '/comments" method="post" id="comment-form">',
+        '<textarea name="comment" class="form-control" rows="3" placeholder="Your Comment"></textarea><br>',
+      '<button type="submit" class="btn btn-primary">Post Comment</button>',
+    '</form>'
+  ].join(' ');
+}
+
+function constructCommentLink(url) {
+  return [
+    '<div class="workout-comment">',
+      '<a href="' + url + '" class="leave-comment">Leave a comment</a>',
+    '</div></div>'
+  ].join('');
+}
+
+function displayError(error) {
+  $('div.field-with-errors').append('<p style="color:red">' + error + '</p>');
+}
+
+function displayComment(comment, url) {
+  $('form').remove();
+  $('.field-with-errors').remove();  $('#browse-plan-table').append(constructComment(comment));
+  $('#browse-plan-table').append(constructCommentLink(url));
+  $('#comment-form')[0].reset();
 }
 
 $(function() {
@@ -58,20 +125,21 @@ $(function() {
 
       // Add comments to workout plan
       if (data.workout_plan.comments.length > 0) {
-        var commentHTML = '<div class="comments-container"><h2 class="comments-header">Comments:</h2><hr>';
-        var allComments = '';
+        var commentsHeader = [
+          '<div class="comments-container">',
+            '<h2 class="comments-header">Comments:</h2><hr>'
+        ].join(' ');
 
+        var allComments = '';
         data.workout_plan.comments.forEach(function(comment){
           allComments += constructComment(comment);
         });
 
-        $('#browse-plan-table').append(commentHTML + allComments);
+        $('#browse-plan-table').append(commentsHeader + allComments);
       };
 
       // Add 'Leave a comment' link
-      var commentLink = '<div class="workout-comment"><a href="' + this.url + '" class="leave-comment">Leave a comment</a></div></div>';
-
-      $('#browse-plan-table').append(commentLink);
+      $('#browse-plan-table').append(constructCommentLink(this.url));
     });
   });
 
@@ -79,16 +147,16 @@ $(function() {
   $('#browse-plan-table').on('click', 'a.leave-comment', function(e){
     e.preventDefault();
     $('a.leave-comment').addClass('hidden');
-    var formAction = $(this).attr('href');
+    var formAction = $(this).attr('href').replace('/comments', '');
 
-    var commentInput = '<div class="field-with-errors"></div><form action="' + formAction + '/comments" method="post" id="comment-form"><textarea name="comment" class="form-control" rows="3" placeholder="Your Comment"></textarea><br><button type="submit" class="btn btn-primary">Post Comment</button></form>'
-
-    $('.workout-comment').html(commentInput);
+    // $('.workout-comment').html(constructCommentForm(formAction));
+    $('#browse-plan-table').append(constructCommentForm(formAction));
   });
 
   // Submit and display comment
   $('#browse-plan-table').on('submit', '#comment-form', function(e){
     e.preventDefault();
+
     var data = $('form').serialize();
     var url = $(this).attr('action');
 
@@ -99,17 +167,13 @@ $(function() {
       dataType: 'json',
       success: function(data){
         if (data.comments) {
-          var html = '<p style="color:red">' + data.comments + '</p>';
-          $('div.field-with-errors').append(html);
+          displayError(data.comments);
         } else {
-          $('form').remove();
-          $('.field-with-errors').remove();
-          $('a.leave-comment').removeClass('hidden');
-          $('#browse-plan-table').append(constructComment(data.comment));
+          displayComment(data.comment, url);
         }
       },
       error: function(XMLHttpRequest, textStatus, errorThrown) {
-        alert("Your commment failed to ");
+        alert("Your comment failed to be posted");
       }
     });
   });
